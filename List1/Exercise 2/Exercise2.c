cb46b903b09c3f857944ac29de074c3b523a8a27
Gustavo Roos <gustavohroos@gmail.com>
2021-08-30 13:56:14 -0300

    ctrl + k + f

--- List1/Exercise 2/Exercise2.c
+++ List1/Exercise 2/Exercise2.c
@@ -1,113 +1,125 @@
 // bibliografia: https://www.geeksforgeeks.org/dynamic-memory-allocation-in-c-using-malloc-calloc-free-and-realloc/
 
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 int menu(void);
 void mostrar(char *nomes, int sizenomes);
-char* adicionar(char *nomes);
-char* remover(char *nomes);
+char *adicionar(char *nomes);
+char *remover(char *nomes);
 
 int sizenomes = 0;
 
-int main(){
+int main()
+{
 
     int escolha;
     char *nomes;
 
     nomes = (char *)calloc(1, 1);
 
-    for(;;){
+    for (;;)
+    {
         escolha = menu();
-        switch (escolha){
-            case 1:
-                nomes = adicionar(nomes);
-                sizenomes++;
-                break;
-            case 2:
-                nomes = remover(nomes);
-                break;
-            case 3:
-                mostrar(nomes, sizenomes);
-                break;
-            case 4:
-                free(nomes);
-                exit(0);
-                break;
-		}
+        switch (escolha)
+        {
+        case 1:
+            nomes = adicionar(nomes);
+            sizenomes++;
+            break;
+        case 2:
+            nomes = remover(nomes);
+            break;
+        case 3:
+            mostrar(nomes, sizenomes);
+            break;
+        case 4:
+            free(nomes);
+            exit(0);
+            break;
+        }
     }
     return 0;
 }
 
-int menu(void){	
+int menu(void)
+{
 
-	int escolha = 0;
-    
-	do{
-		printf("\n1) adicionar nome;\n");
+    int escolha = 0;
+
+    do
+    {
+        printf("\n1) adicionar nome;\n");
         printf("2) remover nome;\n");
-		printf("3) listar;\n");
-		printf("4) sair.\n");
-		printf("\npor favor, digite uma escolha: ");
+        printf("3) listar;\n");
+        printf("4) sair.\n");
+        printf("\npor favor, digite uma escolha: ");
         scanf("%d", &escolha);
-	}while(escolha < 1 || escolha > 4);
-	getchar();
+    } while (escolha < 1 || escolha > 4);
+    getchar();
 
-	return escolha;
+    return escolha;
 }
 
-char* adicionar(char *nomes){
+char *adicionar(char *nomes)
+{
 
     char buffer[20];
 
-    printf("\ndigite o nome a ser adicionado: "); 
+    printf("\ndigite o nome a ser adicionado: ");
     scanf("%s", buffer);
     strcat(buffer, "\n");
 
-    nomes = (char *)realloc(nomes, strlen(nomes)+strlen(buffer)+1);
+    nomes = (char *)realloc(nomes, strlen(nomes) + strlen(buffer) + 1);
     strcat(nomes, buffer);
 
     return nomes;
 }
 
-char* remover(char *nomes){
-    
+char *remover(char *nomes)
+{
+
     char buffer[20];
     int i = 0, size;
 
-    printf("\ndigite o nome a ser removido: "); 
+    printf("\ndigite o nome a ser removido: ");
     scanf("%s", buffer);
 
     char *localizador = strstr(nomes, buffer);
     size = strlen(nomes);
 
-    if(localizador){
+    if (localizador)
+    {
 
-        for(i = 0; i < strlen(buffer)+1; i++){
-            memmove(localizador, (localizador+1), strlen(localizador+1) + 1);
+        for (i = 0; i < strlen(buffer) + 1; i++)
+        {
+            memmove(localizador, (localizador + 1), strlen(localizador + 1) + 1);
         }
 
-        if(size == i){
-            nomes = (char *)realloc(nomes, size-i+2);
+        if (size == i)
+        {
+            nomes = (char *)realloc(nomes, size - i + 2);
             sizenomes--;
             return nomes;
         }
-        nomes = (char *)realloc(nomes, size-i+1);
+        nomes = (char *)realloc(nomes, size - i + 1);
 
         sizenomes--;
-
-    }else{
+    }
+    else
+    {
         printf("\nnome não foi encontrado :(\n");
     }
 
     return nomes;
 }
 
-void mostrar(char *nomes, int sizenomes){
+void mostrar(char *nomes, int sizenomes)
+{
 
-    if (sizenomes == 0) {
+    if (sizenomes == 0)
+    {
         printf("\nnão existem nomes cadastrados ainda!\n");
         return;
     }
@@ -116,6 +128,4 @@ void mostrar(char *nomes, int sizenomes){
 
     printf("\npressione enter para voltar ao menu principal...");
     getchar();
-
-    
 }
