ba13435adb531d909960c6f7bcbea93acabae40d
Gustavo Roos <gustavohroos@gmail.com>
2021-10-10 23:38:34 -0300

    finished avl.c

--- List4/AVL.c
+++ List4/AVL.c
@@ -4,28 +4,27 @@
 
 struct Nodo{
   int chave;
-  int alturaSub;
-  struct Nodo *nEsq;
-  struct Nodo *nDir;
+  int alturaNodo;
+  struct Nodo *nEsq, *nDir;
 };
 
-// typedef struct Nodo Nodo;
+typedef struct Nodo Nodo;
 
-struct Nodo *adicionar(struct Nodo *Nodo, int numero);
-struct Nodo *novoNodo(int chave);
-struct Nodo *rotDir(struct Nodo *Nodo);
-struct Nodo *rotEsq(struct Nodo *Nodo);
-int verificar(struct Nodo *Nodo);
-int alturaSub(struct Nodo *Nodo);
+Nodo *adicionar( Nodo *Nodo, int numero);
+Nodo *novoNodo(int chave);
+Nodo *rotDir( Nodo *Nodo);
+Nodo *rotEsq( Nodo *Nodo);
+int verificar( Nodo *Nodo);
+int alturaNodo( Nodo *Nodo);
 int maximo(int a, int b);
-int calcularFB(struct Nodo *Nodo);
-void limparArv(struct Nodo *raiz);
-void printArv(struct Nodo *Nodo);
+int calcularFB( Nodo *Nodo);
+void limparArv( Nodo *raiz);
+void printar( Nodo *Nodo);
 void segundoCenario();
-void limparNodo(struct Nodo *Nodo);
+void limparNodo( Nodo *Nodo);
 
 int main(){
-  struct Nodo *raiz = NULL;
+   Nodo *raiz = NULL;
   int qtdNodos, numero;
 
   printf("\n-> Digite a quantidade de nodos para inserir: ");
@@ -37,7 +36,7 @@ int main(){
     raiz = adicionar(raiz, numero);
   }
   printf("\n\n");
-  printArv(raiz);
+  printar(raiz);
   printf("\n");
 
   limparArv(raiz);
@@ -46,7 +45,7 @@ int main(){
   return 0;
 }
 
-struct Nodo *adicionar(struct Nodo *Nodo, int numero){
+ Nodo *adicionar( Nodo *Nodo, int numero){
   int fb;
 
   if (Nodo == NULL){
@@ -60,7 +59,7 @@ struct Nodo *adicionar(struct Nodo *Nodo, int numero){
     Nodo->nDir = adicionar(Nodo->nDir, numero);
   }
 
-  Nodo->alturaSub = 1 + maximo(alturaSub(Nodo->nEsq), alturaSub(Nodo->nDir));
+  Nodo->alturaNodo = 1 + maximo(alturaNodo(Nodo->nEsq), alturaNodo(Nodo->nDir));
 
   fb = calcularFB(Nodo);
 
@@ -82,33 +81,34 @@ struct Nodo *adicionar(struct Nodo *Nodo, int numero){
   return Nodo;
 }
 
-struct Nodo *rotDir(struct Nodo *Nodo){
-  struct Nodo *auxLeft = Nodo->nEsq;
-  struct Nodo *auxDir = auxLeft->nDir;
+ Nodo *rotDir(Nodo *Nodo){
+  struct Nodo *auxEsq = Nodo->nEsq;
+  struct Nodo *auxDir = auxEsq->nDir;
 
-  auxLeft->nDir = Nodo;
+  auxEsq->nDir = Nodo;
   Nodo->nEsq = auxDir;
  
-  Nodo->alturaSub = maximo(alturaSub(Nodo->nEsq), alturaSub(Nodo->nDir)) + 1;
-  auxLeft->alturaSub = maximo(alturaSub(auxLeft->nEsq), alturaSub(auxLeft->nDir)) + 1;
+  Nodo->alturaNodo = maximo(alturaNodo(Nodo->nEsq), alturaNodo(Nodo->nDir)) + 1;
+  auxEsq->alturaNodo = maximo(alturaNodo(auxEsq->nEsq), alturaNodo(auxEsq->nDir)) + 1;
 
-  return auxLeft;
+  return auxEsq;
 }
 
-struct Nodo *rotEsq(struct Nodo *Nodo){
+ Nodo *rotEsq(Nodo *Nodo){
+
   struct Nodo *auxDir = Nodo->nDir;
-  struct Nodo *auxLeft = auxDir->nEsq;
+  struct Nodo *auxEsq = auxDir->nEsq;
 
   auxDir->nEsq = Nodo; 
-  Nodo->nDir = auxLeft;
+  Nodo->nDir = auxEsq;
 
-  Nodo->alturaSub = maximo(alturaSub(Nodo->nEsq), alturaSub(Nodo->nDir)) + 1;
-  auxDir->alturaSub = maximo(alturaSub(auxDir->nEsq), alturaSub(auxDir->nDir)) + 1;
+  Nodo->alturaNodo = maximo(alturaNodo(Nodo->nEsq), alturaNodo(Nodo->nDir)) + 1;
+  auxDir->alturaNodo = maximo(alturaNodo(auxDir->nEsq), alturaNodo(auxDir->nDir)) + 1;
 
   return auxDir;
 }
 
-int verificar(struct Nodo *Nodo){
+int verificar(Nodo *Nodo){
   int fb;
 
   if (Nodo == NULL){
@@ -129,43 +129,43 @@ int verificar(struct Nodo *Nodo){
   }
 }
 
-int alturaSub(struct Nodo *Nodo){
+int alturaNodo(Nodo *Nodo){
   if (Nodo == NULL){
     return 0;
   }
-  return Nodo->alturaSub;
+  return Nodo->alturaNodo;
 }
 
 int maximo(int a, int b){
   return (a > b) ? a : b;
 }
 
-int calcularFB(struct Nodo *Nodo){
+int calcularFB(Nodo *Nodo){
   if (Nodo == NULL){
     return 0;
   }
-  return (alturaSub(Nodo->nEsq) - alturaSub(Nodo->nDir));
+  return (alturaNodo(Nodo->nEsq) - alturaNodo(Nodo->nDir));
 }
 
-struct Nodo *novoNodo(int chave){
-  struct Nodo *Nodo = (struct Nodo *)calloc(1, sizeof(struct Nodo));
+Nodo *novoNodo(int chave){
+  Nodo *Nodo = (struct Nodo *)calloc(1, sizeof( Nodo));
   Nodo->chave = chave;
   Nodo->nEsq = NULL;
   Nodo->nDir = NULL;
-  Nodo->alturaSub = 1;
+  Nodo->alturaNodo = 1;
   return Nodo;
 }
 
-void printArv(struct Nodo *Nodo){
+void printar(Nodo *Nodo){
   if (Nodo != NULL){
     printf("(%d", Nodo->chave);
-    printArv(Nodo->nEsq);
-    printArv(Nodo->nDir);
+    printar(Nodo->nEsq);
+    printar(Nodo->nDir);
     printf(")");
   }
 }
 
-void limparNodo(struct Nodo *Nodo){
+void limparNodo(Nodo *Nodo){
   if (Nodo != NULL){
     limparNodo(Nodo->nEsq);
     limparNodo(Nodo->nDir);
@@ -173,7 +173,7 @@ void limparNodo(struct Nodo *Nodo){
   }
 }
 
-void limparArv(struct Nodo *raiz){
+void limparArv(Nodo *raiz){
   if (raiz != NULL){
     limparNodo(raiz);
   }
@@ -181,16 +181,16 @@ void limparArv(struct Nodo *raiz){
 
 void segundoCenario(){
   printf("Primeiro caso: \n");
-  struct Nodo *raiz1 = NULL;
+  Nodo *raiz1 = NULL;
 
   raiz1 = adicionar(raiz1, 20);
   raiz1 = adicionar(raiz1, 4);
   raiz1 = adicionar(raiz1, 15);
-  printArv(raiz1);
+  printar(raiz1);
   limparArv(raiz1);
 
   printf("\nSegundo caso: \n");
-  struct Nodo *raiz2 = NULL;
+  Nodo *raiz2 = NULL;
 
   raiz2 = adicionar(raiz2, 20);
   raiz2 = adicionar(raiz2, 4);
@@ -198,11 +198,11 @@ void segundoCenario(){
   raiz2 = adicionar(raiz2, 3);
   raiz2 = adicionar(raiz2, 9);
   raiz2 = adicionar(raiz2, 15);
-  printArv(raiz2);
+  printar(raiz2);
   limparArv(raiz2);
 
   printf("\nTerceiro caso: \n");
-  struct Nodo *raiz3 = NULL;
+  Nodo *raiz3 = NULL;
 
   raiz3 = adicionar(raiz3, 20);
   raiz3 = adicionar(raiz3, 4);
@@ -215,7 +215,7 @@ void segundoCenario(){
   raiz3 = adicionar(raiz3, 7);
   raiz3 = adicionar(raiz3, 11);
   raiz3 = adicionar(raiz3, 15);
-  printArv(raiz3);
+  printar(raiz3);
   limparArv(raiz3);
 
   printf("\n\n");
