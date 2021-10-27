c0fd6da20910b971eea9dd95894b6c29cc66ec2b
Gustavo Roos <gustavohroos@gmail.com>
2021-10-27 16:32:35 -0300

    finished

--- List5/exercise.c
+++ List5/exercise.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <time.h> //clock(), CLOCKS_PER_SEC e clock_t
 
 void insertionSort(int *vetor, int n);
 void selectionSort(int *vetor, int n);
-void quickSort(int *vetor, int ladoEsq, int ladoDir);
-void mergeSort(int *vetor, int inicio, int fim);
+void quickSort(int *vetor, int esquerda, int direita);
+void mergeSort(int *vetor, int esquerda, int direita);
 void merge(int *vetor, int inicio, int meio, int fim);
 void printarVetor(int *vetor, int n);
 void verificar(int *vetor, int n);
+void swap(int *primeiro, int *segundo);
 
 int main(){
     int *vetorInicial, *vetorInsertionSort, *vetorSelectionSort, *vetorMergeSort,
@@ -110,6 +112,7 @@ void verificar(int *vetor, int n){
 }
 
 void insertionSort(int *vetor, int n){
+
     int aux = 0, j =  0;
 
     for(int i = 1; i < n; i++){
@@ -122,6 +125,7 @@ void insertionSort(int *vetor, int n){
 }
 
 void selectionSort(int *vetor, int n){
+
     int aux1, aux2; //aux2 = menor valor
 
     for(int i = 0; i < (n - 1); i++){
@@ -131,23 +135,97 @@ void selectionSort(int *vetor, int n){
                 aux2 = j;
             }
         }
-        if(i != aux2){
-            aux1 = vetor[i];
-            vetor[i] = vetor[aux2];
-            vetor[aux2] = aux1;
-        }
+        swap(&vetor[aux2], &vetor[i]);
     }
 }
 
-void quickSort(int *vetor, int ladoEsq, int ladoDir){
+void quickSort(int *vetor, int inicio, int fim){
+
+    int pivo, aux;
+    int i = inicio;
+    int j = fim;
+
+    pivo = vetor[(inicio + fim) / 2];
+    do{
+        while (vetor[i] < pivo){
+            i++;
+        }
+        while (pivo < vetor[j]){
+            j--;
+        }
+
+        if(i <= j){
+            swap(&vetor[i], &vetor[j]);
+            i++;
+            j--;
+        }
+        
+    } while (i <= j);
+    if(inicio < j){
+        quickSort(vetor, inicio, j);
+    }
+    if(i < fim){
+        quickSort(vetor, i, fim);
+    }
+    
 
 
 }
-void mergeSort(int *vetor, int inicio, int fim){
 
-
+void swap(int *primeiro, int *segundo){ 
+    int aux = *primeiro;
+    *primeiro = *segundo;
+    *segundo = aux;
 }
-void merge(int *vetor, int inicio, int meio, int fim){
 
 
+void mergeSort(int *vetor, int esquerda, int direita){
+
+    int pivo;
+    if(esquerda < direita){
+        pivo = floor((esquerda + direita) / 2);
+        mergeSort(vetor, esquerda, pivo);
+        mergeSort(vetor, (pivo + 1), direita);
+        merge(vetor, esquerda, pivo, direita);
+    }
+}
+
+void merge(int *vetor, int esquerda, int pivo, int direita){
+
+    int *aux, auxEsquerda, auxDireita, tam, j, k;
+    int fim1 = 0, fim2 = 0;
+    tam = direita - esquerda + 1;
+    auxEsquerda = esquerda;
+    auxDireita = pivo + 1;
+    aux = (int *)malloc(sizeof(int) * tam);
+    if(aux != NULL){
+        for(int i = 0; i < tam; i++){
+            if(!fim1 && !fim2){
+                if(vetor[auxEsquerda] < vetor[auxDireita]){
+                    aux[i] = vetor[auxEsquerda + 1];
+                }
+                else{
+                    aux[i] = vetor[auxDireita + 1];
+                }
+                if(auxEsquerda > pivo){
+                    fim1 = 1;
+                }
+                if(auxDireita > direita){
+                    fim2 = 1;
+                }
+            }
+            else{
+                if(!fim1){
+                    aux[i] = vetor[auxEsquerda + 1];
+                }
+                else{
+                    aux[i] = vetor[auxDireita + 1];
+                }
+            }
+        }
+        for(j = 0, k = esquerda; j < tam; j++, k++){
+            vetor[k] = aux[j];
+        }
+    }
+    free(aux);
 }
